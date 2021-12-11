2a97c5f273c6c2af67e15293b620d9219d2ee040
Shelgir <shelgirr@gmail.com>
2021-12-11 15:16:26 +0300

    register route done

--- src/routes/Auth.Routes.js
+++ src/routes/Auth.Routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
 import Users from "../models/UserModel.js";
+import userValid from "../validations/UserValidate.js";
 
 const AuthRouter = Router();
 
 AuthRouter.post("/login", async (req, res) => {
-  console.log(req.body, process.env.JWT_PRIVATE_KEY);
   let user;
   try {
     user = await Users.findOne({ email: req.body.email });
@@ -14,11 +14,35 @@ AuthRouter.post("/login", async (req, res) => {
   }
 
   if (user) {
-    var token = jwt.sign(req.body, process.env.JWT_PRIVATE_KEY);
-    console.log(token);
-    res.send({ bearer: token });
+    if (user.password === req.body.password) {
+      var token = jwt.sign(req.body, process.env.JWT_PRIVATE_KEY);
+
+      res.send({ bearer: token });
+    } else {
+      return res.send("Password Incorrect");
+    }
   } else {
     return res.send("User Not Found");
+  }
+});
+
+AuthRouter.post("/register", async (req, res) => {
+  // try validate akain agar validate bu anja ache addi user bka, agin err message return akaw function awaste
+  let user;
+  try {
+    await userValid.validateAsync(req.body);
+    user = await Users.findOne({ email: req.body.email });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+
+  if (user === null) {
+    const { body } = req;
+    console.log(body);
+    const newUser = new Users(body);
+    newUser.save().then((e) => res.send(e));
+  } else {
+    return res.send("this email is associated with an account already");
   }
 });
 
